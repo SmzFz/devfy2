4007f742da7ca7398f6df4547e3236d418529270
SmzFz <felipesalmazo123@gmail.com>
2023-11-01 14:17:32 -0300

    modelo inicial devfy2

---
+++ client/src/pages/App.jsx
@@ -0,0 +1,19 @@
+import "./app.css"
+
+const App = () => {
+  return (
+    <>
+    <div className="App">
+      <div className="sidebar">
+ <p>sidebar</p>
+      </div>
+      <div className="content">
+<p>Conteudo</p>
+      </div>
+    </div>
+     <footer><p>Rodapé</p></footer>
+    </>
+  )
+}
+
+export default App
