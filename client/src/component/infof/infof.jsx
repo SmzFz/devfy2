63a56632794972c6f9b47dd8c911c28d43d4b6f0
Felipe Salmazo <126248721+SmzFz@users.noreply.github.com>
2023-11-07 03:04:40 +0000

    finalizando footer

--- client/src/component/infof/infof.jsx
+++ client/src/component/infof/infof.jsx
@@ -3,7 +3,14 @@ import styles from "./infof.module.css"
 const Infof = () => {
   return (
     <div className={styles.container}>
+<div className={styles.escrito}> 
+<h2>Preview of Spotify</h2>
+<p>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</p>
+</div>
+<div className={styles.buttonf}>  
+<button>Sing up Free</button>
 
+</div>
     </div>
   )
 }
